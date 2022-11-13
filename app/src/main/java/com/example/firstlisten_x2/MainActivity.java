d2266da2c82cb79c9273a218c20a161c0e64cdf8
nitair <nitairynar@gmail.com>
2022-11-13 14:57:35 +0200

    סיימתי, מקבל שני שמות ומחזיר שם כולל

--- app/src/main/java/com/example/firstlisten_x2/MainActivity.java
+++ app/src/main/java/com/example/firstlisten_x2/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.firstlisten_x2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText first, second;
+    TextView fullName;
+    Button save;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        first = (EditText)findViewById(R.id.firstName);
+        second = (EditText) findViewById(R.id.lastName);
+        fullName = (TextView) findViewById(R.id.text);
+        save = (Button) findViewById(R.id.save);
+
+        save.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if(view == save)
+        {
+            fullName.setText(first.getText().toString() + " " + second.getText().toString());
+            first.setText("");
+            second.setText("");
+
+        }
+
+
+
     }
 }
